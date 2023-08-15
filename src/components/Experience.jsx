b31a3260c68eb628791e797950b3f1ba15c09509
MahmoudGamal <gemy.bgf@gmail.com>
2023-08-15 12:52:07 +0300

    ''

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -31,11 +31,11 @@ const Experience = () => {
     return ( 
     <div name='experience' className='bg-gradient-to-b from-gray-800 via-black to-black w-full lg:h-screen'>
         
-        <div className='max-w-screen-lg mx-auto p-4 flex flex-col  justify-around w-full h-full text-white'>
+        <div className='max-w-screen-lg mx-auto p-4 flex flex-col  justify-around w-full text-white'>
             <div className='flex flex-col justify-center items-center'>
                 <p className='text-6xl font-signature font-bold   p-2 inline'>My Favourite Assistants</p>
             </div>
-            <div className=' w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-0 sm:gap-5 md:gap-10 lg:gap-15 text-center py-8 px-12 sm:px-0'>
+            <div className=' w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-0 sm:gap-5 md:gap-10 lg:gap-15 text-center py-8 px-12 sm:px-0'>
 
                 {technologies.map(({id,imgData,name,style})=>{
                     return (
