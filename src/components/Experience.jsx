e8745061da5e50c75e9bc68feee63916bb4e0959
MahmoudGamal <gemy.bgf@gmail.com>
2023-08-15 16:27:46 +0300

    added gradient header

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -29,17 +29,18 @@ const Experience = () => {
 
 
     return ( 
-    <div name='experience' className='bg-gradient-to-b from-gray-800 via-black to-black w-full lg:h-screen'>
+    <div name='experience' className='bg-gradient-to-b from-gray-800 via-black to-black  lg:h-screen'>
         
-        <div className='max-w-screen-lg mx-auto p-4 flex flex-col  justify-around w-full text-white'>
-            <div className='flex flex-col justify-center items-center'>
-                <p className='text-6xl font-signature font-bold   p-2 inline'>My Favourite Assistants</p>
+        <div className='max-w-screen-lg mx-auto p-4 flex flex-col  justify-around  h-full text-white'>
+            <div className='flex flex-col justify-center items-center overflow-hidden'>
+                <p className=' text-6xl font-signature font-bold p-2 inline'>My Favourite Assistants</p>
+                <span className=' opacity-90 absolute left-[580px] animate-bounce duration-1000 translate-x-[900px] top-0 bg-gradient-to-r via-red-800 to-red-900 h-[300px] w-[300px]'></span>
             </div>
-            <div className=' w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-0 sm:gap-5 md:gap-10 lg:gap-15 text-center py-8 px-12 sm:px-0'>
+            <div className=' w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-0 sm:gap-5 md:gap-10 lg:gap-15 text-center py-8 px-12 sm:px-0'>
 
                 {technologies.map(({id,imgData,name,style})=>{
                     return (
-                        <div key={id} className={` flex flex-col justify-between shadow-md  duration-500 py-2 rounded-lg ${style}`}>
+                        <div key={id} className={` flex flex-col justify-between hover:animate-pulse shadow-md  duration-500 py-2 rounded-lg ${style}`}>
                             <img src={imgData} alt="random" className=' hover:scale-105 duration-300 rounded-t-lg  w-[100%] h-[65%] mx-auto' />
                             <div className='flex h-full justify-center items-center'>
                                 <p className=' font-bold text-4xl font-signature mt-4'>{name}</p>
